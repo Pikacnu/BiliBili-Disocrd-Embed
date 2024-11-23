bc30ea9873b892ede53bfbf51b2b3a9d0a045933
Pikacnu <92923578+Pikacnu@users.noreply.github.com>
2024-11-24 02:43:35 +0800

    feat: add pipe version of embed-bilibili (long video might not supported on embed)

--- linux.ts
+++ linux.ts
@@ -1,7 +1,9 @@
-import { readFile } from 'fs/promises';
-import YTDlpWrap from 'yt-dlp-wrap';
 import { mkdir, exists } from 'fs/promises';
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
+import { spawn } from 'child_process';
+import { Readable, Writable } from 'stream';
+import { writeFile } from 'fs/promises';
+import Bun from 'bun';
 
 if (!(await exists('./download'))) {
 	await mkdir('./download');
@@ -12,28 +14,106 @@ enum VideoType {
 	Bangumi = '/bangumi/play',
 }
 
+async function streamMergedVideo(
+	id: string,
+	type: VideoType,
+): Promise<Response> {
+	console.log('Streaming merged video live:', id);
+
+	const videoStream = spawn(
+		'yt-dlp',
+		`-f bestvideo -o - https://www.bilibili.com${type}/${id}`.split(' '),
+		{
+			stdio: ['pipe', 'pipe', 'ignore'],
+		},
+	);
+	const audioStream = spawn(
+		`yt-dlp`,
+		`-f bestaudio -o - https://www.bilibili.com${type}/${id}`.split(' '),
+		{
+			stdio: ['pipe', 'pipe', 'ignore'],
+		},
+	);
+
+	// 合併流
+	const ffmpeg = spawn(
+		`ffmpeg`,
+		`-i pipe:0 -thread_queue_size 1024 -i pipe:3 -filter_complex "[0:v] [1:v] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -shortest -thread_queue_size 1024 -c:v copy -c:a aac -movflags frag_keyframe+empty_moov -f mp4 pipe:1`.split(
+			' ',
+		),
+		{
+			stdio: ['pipe', 'pipe', 'ignore', 'pipe'], // 配置 stdio 管道
+			shell: true,
+		},
+	);
+
+	ffmpeg.unref();
+	videoStream.on('error', (err) => {
+		console.error('videoStream error', err);
+	});
+	(videoStream.stdout as Readable).pipe(ffmpeg.stdio[0] as Writable, {
+		end: false,
+	});
+	(audioStream.stdout as Readable).pipe(ffmpeg.stdio[3] as Writable, {
+		end: false,
+	});
+	// 將影片和音訊流寫入 ffmpeg
+
+	writeFile(
+		`./download/${id}/video.mp4`,
+		ffmpeg.stdio[1] as Readable,
+		'binary',
+	);
+
+	// 返回合併流
+	return new Response(
+		new ReadableStream({
+			start(controller) {
+				(ffmpeg.stdio[1] as Readable).on('data', (chunk) => {
+					controller.enqueue(chunk);
+				});
+				(ffmpeg.stdio[1] as Readable).on('end', () => {
+					controller.close();
+				});
+			},
+		}),
+		{
+			headers: {
+				'Content-Type': 'video/mp4',
+				'Cache-Control': 'no-cache,max-age=0',
+			},
+		},
+	);
+}
+
 async function downloadVideo(id: string, type: VideoType = VideoType.Video) {
-	console.log('Downloading', id);
 	console.log('Type', type);
 	if (
 		!(await exists(`./download/${id}`)) ||
 		!(await exists(`./download/${id}/video.mp4`))
 	) {
 		if (!(await exists(`./download/${id}`))) await mkdir(`./download/${id}`);
-
-		const ytdlp = new YTDlpWrap();
-		await ytdlp.execPromise([
-			'--write-thumbnail',
-			'--write-info-json',
-			'-o',
-			`${__dirname}/download/${id}/video.%(ext)s`,
-			`https://www.bilibili.com${type}/${id}`,
-		]);
+		await new Promise((resolve, reject) => {
+			spawn('yt-dlp', [
+				'--write-thumbnail',
+				'--write-info-json',
+				'-o',
+				`${__dirname}/download/${id}/video.%(ext)s`,
+				`https://www.bilibili.com${type}/${id}`,
+			]).on('exit', (code) => {
+				if (code === 0) {
+					resolve(void 0);
+				} else {
+					reject(code);
+				}
+			});
+		});
 	}
 
 	const video_info = JSON.parse(
 		(await readFile(`./download/${id}/video.info.json`)).toString(),
 	);
+	console.log('Send Embed html', id);
 	return new Response(
 		(await readFile('./embed.html'))
 			.toString()
@@ -41,7 +121,7 @@ async function downloadVideo(id: string, type: VideoType = VideoType.Video) {
 			.replaceAll('@@thumbnail_url@@', `/thumbnail/${id}`)
 			.replaceAll(
 				'@@video_player@@',
-				`/player/${id}` /*`https://www.bilibili.com/blackboard/newplayer.html?bvid=${id}&danmaku=0&autoplay=1`*/,
+				`https://www.bilibili.com${type}/${id}` /* `/player/${id}` `https://www.bilibili.com/blackboard/newplayer.html?bvid=${id}&danmaku=0&autoplay=1`*/,
 			)
 			.replaceAll('@@title@@', video_info.title)
 			.replaceAll('@@description@@', video_info.description)
@@ -60,6 +140,7 @@ async function downloadVideo(id: string, type: VideoType = VideoType.Video) {
 
 Bun.serve({
 	port: 5173,
+	idleTimeout: 1000 * 60 * 60 * 24,
 	/*
 	hostname: '0.0.0.0',
 	key: readFileSync('./key.pem'),
@@ -72,18 +153,29 @@ Bun.serve({
 		if (url.pathname.startsWith('/video_data')) {
 			const id = url.pathname.split('/').pop();
 			const file = `./download/${id}/video.mp4`;
+			if (!id || /\/\S{12}\//.test(id)) {
+				return new Response('404 Not Found PABAO', { status: 404 });
+			}
 			if (await exists(file)) {
+				console.log('File exists:', file);
 				return new Response(Bun.file(file), {
 					status: 200,
 					headers: {
 						'Content-Type': 'video/mp4',
 						'Cache-Control': 'no-cache,max-age=0',
+						'Content-Disposition': `attachment; filename="${id}.mp4"`,
+						'Content-Ranges': `bytes */${Bun.file(file).size}`,
 					},
 				});
 			}
+			console.log('Streaming merged video:', id);
+			return await streamMergedVideo(id, VideoType.Video);
 		}
 		if (url.pathname.startsWith('/video')) {
 			const id = url.pathname.split('/')[2];
+			if (!id || /\/\S{12}\//.test(id)) {
+				return new Response('404 Not Found PABAO', { status: 404 });
+			}
 			return await downloadVideo(id);
 		}
 		if (url.pathname.startsWith('/bangumi')) {
